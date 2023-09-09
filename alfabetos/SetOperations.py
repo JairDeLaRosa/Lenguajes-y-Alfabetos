7ea9e0e2dfd078ab6bf7dd4e90cf07dca4e9f12f
yeiler <yeiler2209@gmail.com>
2023-09-09 12:56:43 -0500

    code changues V2

--- alfabetos/SetOperations.py
+++ alfabetos/SetOperations.py
@@ -1,14 +1,44 @@
 class SetOperations:
+    
     def __init__(self, alphabet1,alphabet2):
-        self.alphabet1=alphabet1
-        self.alphabet2=alphabet2
-    def union(self):
-        return self.a1
+          def __init__(self):
+            self.list = []
+
+    def add_index(self, index):
+        self.list.append(index)
+
+    def get_index(self, index):
+        return self.list[index]
+
+    def set_list(self, list):
+        self.list = list
     
-    def interception(self):
-        return self.a2
+    def get_list(self):
+        return self.list
     
-    def difference(self):
-        return self.a1 | self.a2
+    def union(self, index1, index2):
+        list_result = set(self.list[index1]) | set(self.list[index2])
+        return list(list_result)
+    
+    def difference(self, index1, index2):
+        list_result = []
+        for index in self.list[index1]:
+            if index not in self.list[index2]:
+                list_result.append(index)
+        return list_result
+        
+    def interception(self, index1, index2):
+        list_result = []
+        for index in self.list[index1]:
+            if index in self.list[index2]:
+                list_result.append(index)
+        return list_result
     
 
+class Alphabets(SetOperations):
+    def star_lock():
+        pass
+    
+    
+class Language(SetOperations):
+        pass
