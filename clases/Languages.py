ff9645435af1338cc254e9aa9c60340085b2cd32
JairDeLaRosa <128413355+JairDeLaRosa@users.noreply.github.com>
2023-09-12 08:57:54 -0500

    changes

--- clases/Languages.py
+++ clases/Languages.py
@@ -7,11 +7,15 @@ class Language(Operations):
     def cardinality_language(language):
         return len(language)   
         
-    def concatenate_languages(self):
+    def concatenate_languages(self, language1, language2):
         concatenate_language=[]
-        for i in self.list:
-            concatenate_language.extend(i)
-            
+        element=''
+        for i in language1:
+            for j in language2:
+                element=f"{i}{j}"
+                element=element.replace('#', '')
+                if element != '':
+                    concatenate_language.append(element)
         return concatenate_language    
     
     def inverse_language(self,language):
@@ -19,30 +23,25 @@ class Language(Operations):
 
         return inverse_laguage
     
-    def pow(self,language, potencia):
-        listaPow=[""]
-        if potencia != 0:
-            for i in range(potencia):
-                for j in list(listaPow):
-                    palabra1 = (j)
-                    for k in list(language):
-                        palabraConcat = palabra1 + (k)
-                        listaPow.append(palabraConcat)
-        listaPow[:0] = '⌀'
-        listaPow = list(set(listaPow))
-        listaPow.sort(key=len)
-        del listaPow[0]
-        return listaPow
+    
+    def pow(self, language, potencia):
+        listaPow=[]
+        pow={}
+        if potencia > 0:
+            listaPow.extend(language)
+            for num in range(potencia-1):
+                listaPow.extend(self.concatenate_languages(listaPow,language))
+                
+        return listaPow        
     
     
-    
-# lista=[]
-# a1=[1, 2, 3, 4]
-# a2=[3, 4, 5]
-# a3=[3,4, 6, 7]
-# lista.append(a1)
-# lista.append(a2)
-# lista.append(a3)
-# print(lista)
-# conjunto=Language(lista)
-# print(conjunto.concatenate_languages())
+lista=[]
+a1=['ab','cd', 'dc'] 
+a2=[3, 4, 5]
+a3=[3,4, 6, 7]
+lista.append(a1)
+lista.append(a2)
+lista.append(a3)
+print(lista)
+conjunto=Language(lista)
+print(conjunto.pow(a1,3))
