21184ec2ff8d2bc962a6b5e9a8b98ccb4b762bfe
yeiler <yeiler2209@gmail.com>
2023-09-11 22:38:02 -0500

    changues

--- alfabetos/SetOperations.py
+++ alfabetos/SetOperations.py
@@ -1,83 +1,91 @@
-import random #languaje
-import itertools #language
+import random  # languaje
+import itertools  # language
+
+
 class SetOperations:
-    def __init__(self, alphabet1,alphabet2):
-          def __init__(self):
-            self.list = []
+    def __init__(self):
+        self.list = []
 
-    def add_index(self, index):
-        self.list.append(index)
+    def add_element(self, elemnt):
+        self.list.append(elemnt)
 
-    def get_index(self, index):
-        return self.list[index]
+    def get_element(self, element):
+        return self.list[element]
 
     def set_list(self, lista):
         self.list = lista
-    
+
     def get_list(self):
         return self.list
-    
+
     def union(self, index1, index2):
         list_result = set(self.list[index1]) | set(self.list[index2])
         return list(list_result)
-    
+
     def difference(self, index1, index2):
         list_result = []
         for index in self.list[index1]:
             if index not in self.list[index2]:
                 list_result.append(index)
         return list_result
-        
+
     def interception(self, index1, index2):
         list_result = []
         for index in self.list[index1]:
             if index in self.list[index2]:
                 list_result.append(index)
         return list_result
-    
+    def mostrar(self):
+        i = 0
+        while i < len(self.list):  # Cambiar la condición a i < len(self.list)
+            print(f"{i}: {self.list[i]}")
+            i += 1
+            
+            
+            
+              
+            
+            
+
 
 class Alphabets(SetOperations):
-    def star_lock(self, index, cant_word):
-        list_result=[]
-        Alphabet=self.list[index]
-        i=0
-        while i in cant_word:
-            word=''
-            for j in range(random.randint(2,5)):
-                word += str(random.choice(Alphabet))
-            if(word not in list_result):
-                list_result.append(word)    
-            else:
-                i-=1    
-            
-            i+=1    
+   def star_lock(self, index, cant_word):
+      list_result = []
+      Alphabet = self.list[index]
+      Alphabet_list = list(Alphabet)  # Convertir el conjunto en una lista
+      i = 0
+      while i < cant_word:
+          word = ""
+          for j in range(random.randint(2, 5)):
+              word += str(random.choice(Alphabet_list))  # Usar la lista en lugar del conjunto
+          if word not in list_result:
+              list_result.append(word)
+              i += 1
+   
+      return list_result
 
-        return list_result
-    
-    
+
 class Language(SetOperations):
-    
     def create_language(alphabet, number):
         symbols1 = alphabet[0]
         symbols2 = alphabet[1]
         language = []
-        
+
         for _ in range(number):
             symbol1 = random.choice(symbols1)
             symbol2 = random.choice(symbols2)
-            word = F"{symbol1}{symbol2}"
+            word = f"{symbol1}{symbol2}"
             language.append(word)
-        
+
         return language
-    
+
     def cardinality_language(language):
         cont = len(language)
         return cont
 
-    
-    def concatenate_languages(language1, language2):        
+    def concatenate_languages(language1, language2):
         concatenate_languages = language1
-        #extend Used to concatenate iterables
+        # extend Used to concatenate iterables
         concatenate_languages = concatenate_languages.extend(language2)
 
         return concatenate_languages
@@ -86,12 +94,12 @@ class Language(SetOperations):
         inverse_laguage = list(reversed(languange))
 
         return inverse_laguage
-    
+
     def power_language(langauge, exponent):
         if exponent < 0:
             return None
-        
-        #itertools.product to generate all possible combinations of language strings to the specified power.
+
+        # itertools.product to generate all possible combinations of language strings to the specified power.
         combinations = itertools.product(langauge, exponent)
 
         for combination in combinations:
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from alfabetos.SetOperations import SetOperations, Language
 from alfabetos.SetOperations import SetOperations,Language,Alphabets
 import os, time
 
@@ -8,124 +7,64 @@ def enterAlphabets(N):
     for i in range(i,N):
         alphabet=set(input(f"ingrese el alfabeto {i + 1} separado por espacios: ").split(" "))
         listAlphabets.append(alphabet)
- 
     return listAlphabets
 
-alphabets=[]
-
-<<<<<<< HEAD
-lista = SetOperations
-
-
-
-
-
-
-# while True:
-#     os.system("cls")
-#     print("""
-#         1-ingresa alfabeto
-#         2-mostrar
-#         0-salir
-#     """)
-#     opcion = int(input("Ingrese la opcion: "))
-#     match opcion:
-#         case 1:
-#             cant_alfabetos = int(input("Ingrese la cantidad de alfabetos a ingresar: "))
-#             alphabets = enterAlphabets(cant_alfabetos)
-
-#         case 2:
-#             print("alfabetos...", alphabets)
-#             time.sleep(2)
-
-#         case 0:
-#             break
-#         case _:
-#             print("opcion invalida!")
-#             input("presione para continuar")
-=======
+alphabets = Alphabets()
 
 while True:
     os.system("cls")
     print("""
-          1-Ingresar alfabetos
-          2-Crear lenguajes
-          3-Opciones de alfabetos
-          4-Opciones de lenguajes
+        1-ingresa alfabeto
+        2-mostrar alfabeto por indice
+        3-unir alfabetos por indice
+        4-diferencia alfabeto por indice
+        5-intercepcion de alfabeto por indice
+        6-cerradura de estrella de alfabeto
+        0-salir
     """)
     opcion = int(input("Ingrese la opcion: "))
     match opcion:
-        case 1:
+        case 1:#ingresar alfabeto
             cant_alfabetos = int(input("Ingrese la cantidad de alfabetos a ingresar: "))
-            alphabets = enterAlphabets(cant_alfabetos)
-   
-        case 2:
-            #Importan !! alphabet is a set of sets and to use this function it must be a list of lists.
-            languages=[]
-            alphabets = [list(subset) for subset in alphabets]
-            number = int(input("Enter how many words you want the language to have: "))
-            language = Language.create_language(alphabets, number)   
-            languages.append(language) 
-            language = Language.create_language(alphabets, number)   
-            languages.append(language)           
-        case 3:
-            while True:
-                os.system("cls")
-                print("""
-                      1-Mostrar Alfabetos
-                      2-Calcular unión
-                      3-Calcular diferencia
-                      4-Calcular intersección
-                      5-Cerradura de estrella
-                      0-Regresar
-                """)
-                opcion = int(input("Ingrese la opcion: "))
-                match opcion:
-                    case 1:
-                        os.system("cls")
-                        print("alfabetos...", alphabets)
-                        time.sleep(15)
-                    case 2:
-                        union_alphabets=Alphabets.union(alphabets[0],alphabets[1])
-                        print(f"La unión de los alfabetos es: {union_alphabets}")
-                        time.sleep(15)
-                    case 3:
-                        difference_alphabets=Alphabets.difference(alphabets[0],alphabets[1])
-                        print(f"La diferenfia de los alfabetos es: {difference_alphabets}")
-                        time.sleep(15)
-                    case 4:
-                        interception_alphabets=Alphabets.interception(alphabets[0],alphabets[1])
-                        print(f"La intersección de los alfabetos es: {interception_alphabets}")
-                        time.sleep(15) 
-                    case 5:
-                        for i in alphabets:
-                            number_of_words=int(input('Digite la cantidad de palabras que desea tener la cerradura: '))
-                            star_lock= Alphabets.star_lock(i,number_of_words)
-                            print(star_lock)      
-                    case 0:
-                        break                   
+            Listalphabets = enterAlphabets(cant_alfabetos)
+            for i in Listalphabets: 
+                alphabets.add_element(i) 
+        case 2:#mostrar alfabeto
+            alphabets.mostrar()
+            input('presiona una tecla para continuar...')
+        case 3:#unir
+            index1=int(input('ingresa el primer indice: '))
+            index2=int(input('ingresa el segundo indice: '))
+            union=alphabets.union(index1, index2)
+            #alphabets.add_element(union)
+            print(f"la union de {alphabets.get_element(index1)} y de {alphabets.get_element(index2)} es:")  
+            print(union)
+            input('presiona una tecla para continuar...')
         case 4:
-            while True:
-                os.system("cls")
-                print("""
-                      1-Mostrar lenguajes
-                      0-Regresar
-                """)
-                opcion = int(input("Ingrese la opcion: "))
-                match opcion:
-                    case 1:
-                        os.system("cls")
-                        print("language...", languages)
-                        time.sleep(15)
-                    case 0:
-                        break      
+            index1=int(input('ingresa el primer indice: '))
+            index2=int(input('ingresa el segundo indice: '))
+            diferencia=alphabets.difference(index1,index2)
+            #alphabets.add_element(diferencia)
+            print(f"la union de {alphabets.get_element(index1)} y de {alphabets.get_element(index2)} es:")  
+            print(diferencia)
+            input('presiona una tecla para continuar...')
+        case 5:
+            index1=int(input('ingresa el primer indice: '))
+            index2=int(input('ingresa el segundo indice: '))
+            intercepcion=alphabets.interception(index1,index2)
+            #alphabets.add_element(intercepcion)
+            print(f"la union de {alphabets.get_element(index1)} y de {alphabets.get_element(index2)} es:")  
+            print(intercepcion)
+            input('presiona una tecla para continuar...')
+        case 6:
+            index=int(input('ingresa el indice del alfabeto: '))
+            longitud=int(input('ingresa la longitud de la cadena que desea generar:'))
+            cadena=alphabets.star_lock(index,longitud)
+            #alphabets.add_element(intercepcion)
+            print(f"la cerrradura de estrella del alfabeto es:{cadena}")  
+            input('presiona una tecla para continuar...')    
         case 0:
             break
         case _:
             print("opcion invalida!")
             input("presione para continuar")
->>>>>>> 55479fce439e3b18d3fda63196ec41d9639beba9
-
-
-
-
